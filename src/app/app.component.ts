be4cf7e15e2523bb41ee66556cefd33bece7b9c7
prince <princeparaste78@gmail.com>
2022-01-05 15:12:59 +0530

    Revert "added form module"
    This reverts commit d15f0fb80b72e8f131db2362bbdb21b86f9284bd.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { UserDataServiceService } from './Services/user-data-service.service';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +7,4 @@ import { UserDataServiceService } from './Services/user-data-service.service';
 })
 export class AppComponent {
   title = 'angular-code-step-by-step';
-  tempUsers = [
-    {id:1,name:'Prince',email:'prince@gmail.com'},
-    {id:2,name:'Harry',email:'harry@gmail.com'},
-    {id:3,name:'cody',email:'cody@gmail.com'},
-  ]
-  userData:any
-  userArr:any
-  constructor(private userInfo: UserDataServiceService){
-    userInfo.userService().subscribe((data) => {
-      console.log(data);
-      this.userArr = data
-    })
-  }
-
-  submitFormData(data:any){
-    console.log(data);
-    this.userData = data
-  }
 }
